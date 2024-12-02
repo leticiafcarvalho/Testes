7884b826d67ca77cca9952b2ca11439d08956cba
leticiafcarvalho <leticiacarvalhos2@outlook.com>
2024-12-02 15:12:43 -0300

    chore: adicionar código SQL para o segundo desafio no repositório

---
+++ Teste2.sql
@@ -0,0 +1,58 @@
+CREATE DATABASE Teste;
+
+USE Teste;
+
+CREATE TABLE DiasSemana
+(
+	DiaId		INT,
+	Descricao	VARCHAR(200)
+);
+
+INSERT INTO DiasSemana
+SELECT 1, 'Domingo' UNION ALL
+SELECT 2, 'Segunda' UNION ALL
+SELECT 3, 'Terça'	UNION ALL
+SELECT 4, 'Quarta'	UNION ALL
+SELECT 5, 'Quinta'	UNION ALL
+SELECT 6, 'Sexta'	UNION ALL
+SELECT 7, 'Sábado';
+
+CREATE TABLE HorariosTrabalho 
+(
+	CadastroId	INT,
+	DiaId		INT,
+	Horas		INT
+);
+
+INSERT INTO HorariosTrabalho
+SELECT 1,2,8 UNION ALL
+SELECT 1,3,8 UNION ALL
+SELECT 1,5,8 UNION ALL
+SELECT 1,7,1 UNION ALL
+SELECT 1,7,5 UNION ALL
+SELECT 2,2,4 UNION ALL
+SELECT 2,3,4 UNION ALL
+SELECT 2,4,4 UNION ALL
+SELECT 2,5,4 UNION ALL
+SELECT 2,6,4 UNION ALL
+SELECT 2,7,4 UNION ALL
+SELECT 3,2,6 UNION ALL
+SELECT 3,3,6 UNION ALL
+SELECT 3,4,6 UNION ALL
+SELECT 3,5,5 UNION ALL
+SELECT 3,5,6 UNION ALL
+SELECT 3,6,6;
+
+-- Resolução: 
+
+SELECT  
+	c.CadastroId,
+	d.Descricao AS DiaSemana,
+	COALESCE(SUM(h.Horas), 0) AS TotalHoras
+FROM DiasSemana d
+CROSS JOIN (SELECT DISTINCT CadastroId FROM HorariosTrabalho) c
+LEFT JOIN HorariosTrabalho h ON c.CadastroId = h.CadastroId AND d.DiaId = h.DiaId
+GROUP BY 
+    c.CadastroId, d.DiaId, d.Descricao
+ORDER BY 
+    c.CadastroId, d.DiaId;
